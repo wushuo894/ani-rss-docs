bd9fc185e228095dc08814c9292f25a3765ddfe9
wushuo <wushuo894@outlook.com>
2025-03-12 04:31:41 +0800

    docs

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -44,4 +44,12 @@ watch(isDark, (dark) => {
       "https://giscus.app"
   );
 });
+
+
+// 去除后缀 .html
+let pathname = location.pathname;
+if (pathname.endsWith(".html")) {
+  location.pathname = pathname.substring(0, pathname.length - 5);
+}
+
 </script>
