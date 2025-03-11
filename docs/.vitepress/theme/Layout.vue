6aa8a61f4816da091bcecd57fc6d6a9827cb71ee
wushuo <wushuo894@outlook.com>
2025-03-11 18:04:54 +0800

    docs

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,47 @@
+<!-- Layout.vue -->
+<template>
+  <Layout>
+    <template #doc-after>
+      <div style="margin-top: 24px">
+        <Giscus
+            :key="page.filePath"
+            repo="wushuo894/ani-rss-docs"
+            repo-id="R_kgDONKaoJw"
+            category="Polls"
+            category-id="DIC_kwDONKaoJ84Cn0w0"
+            mapping="url"
+            strict="1"
+            reactions-enabled="1"
+            emit-metadata="0"
+            input-position="bottom"
+            :theme="isDark ? 'dark' : 'light'"
+            lang="zh-CN"
+        />
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<script setup>
+import Giscus from "@giscus/vue";
+import DefaultTheme from "vitepress/theme";
+import {watch} from "vue";
+import {inBrowser, useData} from "vitepress";
+
+const {isDark, page} = useData();
+
+const {Layout} = DefaultTheme;
+
+watch(isDark, (dark) => {
+  if (!inBrowser) return;
+
+  const iframe = document
+      .querySelector("giscus-widget")
+      ?.shadowRoot?.querySelector("iframe");
+
+  iframe?.contentWindow?.postMessage(
+      {giscus: {setConfig: {theme: dark ? "dark" : "light"}}},
+      "https://giscus.app"
+  );
+});
+</script>
