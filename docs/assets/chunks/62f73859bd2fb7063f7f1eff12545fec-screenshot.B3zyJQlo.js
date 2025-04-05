d3f790220b8df785ae8d1760c79f3d3a42fce1ac
wushuo894 <wushuo894@outlook.com>
2025-04-05 06:03:33 +0000

    Update gh-page to output generated at 9afdd19

--- docs/assets/chunks/62f73859bd2fb7063f7f1eff12545fec-screenshot.B3zyJQlo.js
+++
@@ -1,1 +0,0 @@
-const e="/screenshot/62f73859bd2fb7063f7f1eff12545fec-screenshot.png";export{e as _};
---
+++ docs/assets/chunks/62f73859bd2fb7063f7f1eff12545fec-screenshot.C43Hz95i.js
@@ -0,0 +1,1 @@
+const e="/screenshot/62f73859bd2fb7063f7f1eff12545fec-screenshot.webp";export{e as _};
