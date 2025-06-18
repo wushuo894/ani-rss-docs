965d444c22b390f6040bfbf8b2f2b84c57ca25e6
wushuo <wushuo894@outlook.com>
2025-06-18 08:06:59 +0800

    docs

--- docs/components/History.vue
+++ docs/components/History.vue
@@ -7,23 +7,9 @@ import 'markdown-it-github-alerts/styles/github-colors-light.css'
 import 'markdown-it-github-alerts/styles/github-colors-dark-media.css'
 import 'markdown-it-github-alerts/styles/github-base.css'
 import {ElMessage} from "element-plus";
+import {onMounted} from "vue";
 
 let releases = ref([])
-
-fetch('https://api.github.com/repos/wushuo894/ani-rss/releases?page=1&per_page=50')
-    .then(res => res.json())
-    .then(data => {
-      if (data.message) {
-        ElMessage.error(data.message)
-        return
-      }
-      for (let item of data) {
-        let {assets} = item
-        assets = assets.filter(it => !it.name.endsWith('.md5') && !it.name.endsWith('.json'))
-        item['assets'] = assets
-      }
-      releases.value = data
-    })
 
 let formatDate = (dateString) => {
   const date = new Date(dateString);
@@ -50,56 +36,123 @@ md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
 }
 
 md.use(MarkdownItGitHubAlerts)
+
+const loadList = async () => {
+  let res = await fetch(`https://api.github.com/repos/wushuo894/ani-rss/releases?page=${page.value.currentPage}&per_page=${page.value.size}`)
+  let data = await res.json()
+  if (data.message) {
+    ElMessage.error(data.message)
+    return
+  }
+  for (let item of data) {
+    let {assets} = item
+    assets = assets.filter(it => !it.name.endsWith('.md5') && !it.name.endsWith('.json'))
+    item['assets'] = assets
+  }
+  releases.value = data
+
+  let linkHeader = res.headers.get('Link');
+  if (!linkHeader) {
+    return
+  }
+  const links = linkHeader.split(',');
+  for (const link of links) {
+    const [urlPart, relPart] = link.split(';');
+    const cleanUrl = urlPart.trim().replace(/[<>]/g, ''); // 移除 < 和 >
+    const rel = relPart.trim();
+    if (rel !== 'rel="last"') {
+      continue
+    }
+    const pageParam = new URL(cleanUrl).searchParams.get('page');
+    if (!pageParam) {
+      continue
+    }
+    page.value.totalPage = Number.parseInt(pageParam)
+  }
+}
+
+onMounted(() => load())
+
+const load = () => {
+  loading.value = true
+  loadList()
+      .finally(() =>
+          loading.value = false
+      )
+}
+
+const page = ref({
+  currentPage: 1,
+  size: 10,
+  totalPage: 1
+})
+
+const loading = ref(true)
 </script>
 
 <template>
   <h1>更新历史</h1>
-  <div v-if="releases.length === 0" style="margin-top: 50px;">
-    正在获取中, 请坐和放宽
-  </div>
-  <div v-for="(item,index) in releases" :key="item.id" v-else>
-    <h2>
-      <a :href="item['html_url']" target="_blank">
-        {{ item.name }}
-      </a>
-      <Badge v-if="index === 0" type="tip" text="最新版本"/>
-    </h2>
-    <div>
-      <div v-html="md.render(item['body'])" class="markdown-body"></div>
+  <div style="min-height: 300px" v-loading="loading">
+    <ul>
+      <li v-for="(item,index) in releases" :key="item.id">
+        <h2>
+          <a :href="item['html_url']" target="_blank">
+            {{ item.name }}
+          </a>
+          <Badge v-if="index === 0" type="tip" text="最新版本"/>
+        </h2>
+        <div>
+          <div v-html="md.render(item['body'])" class="markdown-body"></div>
+        </div>
+        <div style="overflow-x: auto;" v-if="item['assets'].length">
+          <div style="min-width: 500px;">
+            <table>
+              <thead>
+              <tr>
+                <th scope="col">文件名</th>
+                <th scope="col">大小</th>
+                <th scope="col">下载次数</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="asset in item['assets']">
+                <td>
+                  <a :href="asset['browser_download_url']" target="_blank">{{ asset.name }}</a>
+                </td>
+                <td>{{ Number(asset.size / 1024 / 1024).toFixed(2) }}MB</td>
+                <td>{{ asset['download_count'] }}</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <el-text size="small" v-if="item['assets'].length">
+          {{ formatDate(item['created_at']) }} 共下载
+          {{ item['assets'].reduce((sum, it) => sum + it['download_count'], 0) }}
+          次
+        </el-text>
+      </li>
+    </ul>
+    <div class="pagination">
+      <el-pagination
+          :page-sizes="[10, 20, 30, 40]"
+          v-model:page-size="page.size"
+          v-model:page-count="page.totalPage"
+          v-model:current-page="page.currentPage"
+          :hide-on-single-page="true"
+          @current-change="load"
+          @size-change="() => {
+            page.currentPage = 1
+            load()
+          }"
+          layout="prev, pager, next, sizes"
+      />
     </div>
-    <div style="overflow-x: auto;" v-if="item['assets'].length">
-      <div style="min-width: 500px;">
-        <table>
-          <thead>
-          <tr>
-            <th scope="col">文件名</th>
-            <th scope="col">大小</th>
-            <th scope="col">下载次数</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr v-for="asset in item['assets']">
-            <td>
-              <a :href="asset['browser_download_url']" target="_blank">{{ asset.name }}</a>
-            </td>
-            <td>{{ Number(asset.size / 1024 / 1024).toFixed(2) }}MB</td>
-            <td>{{ asset['download_count'] }}</td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <p
-        v-if="item['assets'].length"
-        style="
-    color: var(--vp-c-text-2);
-    line-height: 32px;
-    font-size: 14px;
-    font-weight: 500;
-    ">
-      {{ formatDate(item['created_at']) }} 共下载
-      {{ item['assets'].reduce((sum, it) => sum + it['download_count'], 0) }}
-      次</p>
   </div>
 </template>
 
+<style>
+.pagination li {
+  margin-top: 0;
+}
+</style>
